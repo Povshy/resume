ca85770acf0f75dd836b1d2dbeea0b712da0c392
Povshy <vladcarrib@gmail.com>
2023-05-12 01:39:00 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,36 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ в місяц',
+  address: 'Ukraine, Kyiv, Kabak Caribbean & Peppers',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'dmytro@mail.com',
+      href: 'mailto:dmytro@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,10 +53,346 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics ( tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.',
+      },
+    },
+
+    footer,
   })
 })
 
 // ================================================================
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 9,
+          isTop: true,
+        },
+        {
+          name: 'VS Code & NPM',
+          point: 8,
+          isTop: false,
+        },
+        {
+          name: 'Git & Terminal',
+          point: 7,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+
+      hobbies: [
+        {
+          name: 'Football',
+          isMain: true,
+        },
+        {
+          name: 'Basketball',
+          isMain: true,
+        },
+        {
+          name: 'Poker',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      education: [
+        {
+          name: 'School 9',
+          isEnd: true,
+        },
+        {
+          name: 'National University of Food Technologies',
+          isEnd: true,
+        },
+        {
+          name: 'it-Brains',
+          isEnd: false,
+        },
+      ],
+
+      certificates: [
+        {
+          name: 'Diplom Krasavchik',
+          id: 888,
+        },
+        {
+          name: 'Diplom Bartender',
+          id: 777,
+        },
+        {
+          name: 'Diplom Karaoke',
+          id: 999,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// =================================================================
+
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.09.2023',
+            to: '22.01.2024',
+          },
+          projectAmount: 2,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://www.thebestmanspeech.com/',
+              about:
+                'Project completion for a final year student of any course is a moment of pride and happiness.',
+              stackAmount: 3,
+              awardAmount: 2,
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Node.js',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Best of the Best',
+                },
+                {
+                  name: 'Best of the Best of the Best',
+                },
+              ],
+            },
+            {
+              name: 'Project two',
+              url: 'https://www.thebestmanspeech.com/',
+              about:
+                'Project completion for a final year student of any course is a moment of pride and happiness.',
+              stackAmount: 3,
+              awardAmount: 2,
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Node.js',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Second of the Best',
+                },
+                {
+                  name: 'Two of the two of the Best',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          position: 'Big Bartender',
+          company: {
+            name: 'Peppers Club',
+            url: 'https://peppersclub.com.ua/',
+          },
+          duration: {
+            from: '05.09.2021',
+            to: null,
+          },
+          projectAmount: 2,
+
+          projects: [
+            {
+              name: 'SIGNATURE PUB COCKTAILS',
+              url: 'https://peppersclub.com.ua/en/menu/cocktails/',
+              about: 'Different drinks for everyone',
+              stackAmount: 3,
+              awardAmount: 2,
+              stacks: [
+                {
+                  name: 'YELLOW SUBMARINE',
+                },
+                {
+                  name: 'BOHEMIAN RAPSODY',
+                },
+                {
+                  name: 'MANDARIN SAUER',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Best drink',
+                },
+                {
+                  name: 'ENGLISH COOLER',
+                },
+              ],
+            },
+            {
+              name: 'CLASSIC COCKTAILS',
+              url: 'https://peppersclub.com.ua/en/menu/cocktails/',
+              about: 'Different drinks for everyone',
+              stackAmount: 3,
+              awardAmount: 2,
+              stacks: [
+                {
+                  name: 'NEGRONI',
+                },
+                {
+                  name: 'OLD FASHIONED',
+                },
+                {
+                  name: 'WATERMELON SOUR',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Classic drink',
+                },
+                {
+                  name: 'Best Music Pub 2022',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          position: 'Chef Bartender',
+          company: {
+            name: 'Caribbean Club',
+            url: null,
+          },
+          duration: {
+            from: '11.12.2013',
+            to: null,
+          },
+          projectAmount: 1,
+
+          projects: [
+            {
+              name: '10 years in the Club',
+              url: 'https://caribbean.com.ua/',
+              about:
+                'Project completion for a final year student of any course is a moment of pride and happiness.',
+              stackAmount: 3,
+              awardAmount: 2,
+              stacks: [
+                {
+                  name: 'People',
+                },
+                {
+                  name: 'Party',
+                },
+                {
+                  name: 'Good time',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Best party 2015',
+                },
+                {
+                  name: 'Spain 2019',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
